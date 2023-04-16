507c31a63c996c26b32430e8ee4e76466d8085f9
vladxdboi <vladiks232@inbox.lv>
2023-04-16 17:33:57 +0300

    obama is not cool

--- hash_substring.py
+++ hash_substring.py
@@ -19,17 +19,18 @@ def print_occurrences(output):
 def get_occurrences(pattern, text):
     p_hash = hash(pattern)
     t_hash = hash(text[:len(pattern)])
+    positions = []
 
     for i in range(len(text) - len(pattern) + 1):
         if text[i:i+len(pattern)] == pattern:
-            print(i, end=' ')
+            positions.append(i)
         else:
             t_hash = hash(text[i+1:i+len(pattern)+1])
 
         if t_hash == p_hash and text[i:i+len(pattern)] != pattern:
             t_hash = hash(text[i:i+len(pattern)])
 
-    print()
+    return positions
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
